5d0493a994e1098e7df4d4fdf27f9f2213ea55cd
czh50501607 <20719czh@sina.com>
2020-09-25 22:39:24 +0800

    简单化

--- service/index.php
+++ service/index.php
@@ -1,12 +1,8 @@
 <?php
-if ($_FILES["file"]["error"] > 0)
-  {
-  echo "Error: " . $_FILES["file"]["error"] . "<br />";
-  }
-else
-  {
-  echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-  echo "Type: " . $_FILES["file"]["type"] . "<br />";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
-  }
+$data = '{
+  "success": '.true.',
+  "message": "you success connected"
+}';
+
+header('Content-type:text/json');
+echo json_encode($data);
